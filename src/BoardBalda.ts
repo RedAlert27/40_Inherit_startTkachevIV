3aeb24e311f2d3e199ed57516cd7305cd7a7b4ce
Igor <igortkachev94@gmail.com>
2026-01-30 21:29:41 +0700

    Update BoardBalda.ts

--- src/BoardBalda.ts
+++ src/BoardBalda.ts
@@ -1,25 +1,39 @@
 import { Board, BoardParam } from "./Board";
 
-
 export class BoardBalda extends Board {
     constructor(
         str: string[] | string = "балда",
         init: boolean = true
     ) {
-        // TODO
-        // Если длина str==5, то дополняет ее 10 пробелами до и после
-        // Вызывает родительский конструктор,
-        //  если init, то дополнительно инициализируются
-        //  статические поля класса
-            super(str)
+        // Если длина str == 5, дополняем пробелами до 25 символов
+        let cellsStr: string
+        
+        if (Array.isArray(str)) {
+            cellsStr = str.join('')
+        } else {
+            cellsStr = str
+        }
 
+        if (cellsStr.length === 5) {
+            cellsStr = "          " + cellsStr + "          "
+        }
+
+        // Инициализация статических полей при необходимости
+        if (init) {
+            ;(BoardBalda as any).row = BoardBaldaParam.row
+            ;(BoardBalda as any).col = BoardBaldaParam.col
+        }
+
+        super(cellsStr)
     }
+
     clone(init: boolean = false): Board {
-        // TODO
-        // Функция должна вернуть копию объекта
-        // Если init, то дополнительно инициализируются
-        //  статические поля класса
-        return this
+        const board = new BoardBalda([...this.cells], init)
+        if (init) {
+            ;(BoardBalda as any).row = BoardBaldaParam.row
+            ;(BoardBalda as any).col = BoardBaldaParam.col
+        }
+        return board
     }
 }
 
