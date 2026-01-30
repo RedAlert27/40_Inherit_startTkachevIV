8797418e2b90d675445813fc8f9a1e4ddcb72465
Igor <igortkachev94@gmail.com>
2026-01-30 21:30:45 +0700

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -3,15 +3,10 @@ import { GameVC } from "./GameVC"
 import { Input } from "./Input"
 import { State } from "./State"
 
-// Класс
 export class Game {
-    // Шаги игры
     steps: State[]
-    // Номер текущей игры
     current: number
-    // Параметры доски (размеры)
     boardParam: BoardParam
-    // интерфейс для пользовательского ввода
     input: Input
 
     constructor(
@@ -20,43 +15,55 @@ export class Game {
         boardParam: BoardParam,
         current: number = 0
     ) {
-        if (Array.isArray(steps))
-            this.steps = steps
-        else
-            this.steps = [steps]
-        this.current = current
+        if (Array.isArray(steps)) {
+            this.steps = steps.map(state => state.clone())
+        } else {
+            this.steps = [steps.clone()]
+        }
+        this.current = Math.min(current, this.steps.length - 1)
         this.boardParam = boardParam
         this.input = input
     }
 
     get state(): State {
-        // TODO
-        // Сеттер должен возвращать текущее состояние игры
-        return this.steps[0]
+        return this.steps[this.current].clone()
     }
 
     clone(): Game {
-        // TODO
-        // Функция должна вернуть копию объекта
-        return this 
+        return new Game(this.steps, this.input, this.boardParam, this.current)
     }
 
     move(index: number): boolean {
-        // TODO        
-        // Определяет, какой символ ходит, и пытается сделать ход 
-        //  с помощью board.move.
-        // Если ход можно сделать, то добавляет  новыу позицию в steps, 
-        //  обновляет current и возвращает true, иначе возвращает false
-        // Нужно учесть, что если вызывалась функция toStep, то 
-        //  current можно указывать не на последний элемент steps
-        return true  
+        const currentState = this.steps[this.current]
+        const boardCopy = currentState.board.clone()
+        
+        const sym = this.input.sym || currentState.sym
+        
+        if (!boardCopy.move(index, sym)) {
+            return false
+        }
+        
+        if (this.current < this.steps.length - 1) {
+            this.steps = this.steps.slice(0, this.current + 1)
+        }
+        
+        const nextSym = currentState.sym
+        const nextState = new State(boardCopy, nextSym)
+        this.steps.push(nextState)
+        this.current = this.steps.length - 1
+        
+        this.input.move()
+        
+        GameVC.draw()
+        return true
     }
 
-    toStep(step: number) {
-         // TODO
-        // Проверяет, что в steps есть элемент с индексом step,
-        //  если нет то возвращает false
-        // Делает current равным step и обновляет свойство cell в board
-        return true  
+    toStep(step: number): boolean {
+        if (step < 0 || step >= this.steps.length) {
+            return false
+        }
+        this.current = step
+        GameVC.draw()
+        return true
     }
 }
