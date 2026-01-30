9646007fe5ef3696b5d472b1287bb3e52b0fee7c
Igor <igortkachev94@gmail.com>
2026-01-30 21:30:11 +0700

    Update BoardTic.ts

--- src/BoardTic.ts
+++ src/BoardTic.ts
@@ -1,25 +1,38 @@
 import { Board, BoardParam } from "./Board";
 
 export class BoardTic extends Board {
-
     constructor(
-        str: string[] | string = ["_________"],
+        str: string[] | string = "_________",
         init: boolean = true        
     ) {
-        // TODO
-        // Если длина str!=9, то используется строка по умолчанию
-        // Вызывает родительский конструктор,
-        //  если init, то дополнительно инициализируются
-        //  статические поля класса
-            super(str)
+        // Если длина str != 9, используем строку по умолчанию
+        let cellsStr: string
+        if (Array.isArray(str)) {
+            cellsStr = str.join('')
+        } else {
+            cellsStr = str
+        }
+        
+        if (cellsStr.length !== 9) {
+            cellsStr = "_________"
+        }
+
+        // Инициализация статических полей при необходимости
+        if (init) {
+            ;(BoardTic as any).row = BoardTicParam.row
+            ;(BoardTic as any).col = BoardTicParam.col
+        }
+
+        super(cellsStr)
     }
 
-    clone(init: boolean=false): Board {
-        // TODO
-        // Функция должна вернуть копию объекта
-        // Если init, то дополнительно инициализируются
-        //  статические поля класса
-        return this
+    clone(init: boolean = false): Board {
+        const board = new BoardTic([...this.cells], init)
+        if (init) {
+            ;(BoardTic as any).row = BoardTicParam.row
+            ;(BoardTic as any).col = BoardTicParam.col
+        }
+        return board
     }
 
     private getLineChar(line: number[]): string[] {
@@ -27,31 +40,31 @@ export class BoardTic extends Board {
     }
 
     private static winPos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ]
 
-    checkWin() {
-        // TODO
-        // Если имеется комбинация из трех одинаковых символов "X" или "0" 
-        //  в линию - возвращает этот символ
-        // Иначе возвращает символ "_"
-        return "_"
+    checkWin(): string {
+        for (const line of BoardTic.winPos) {
+            const [a, b, c] = this.getLineChar(line);
+            if (a !== '_' && a !== ' ' && a === b && b === c) {
+                return a;
+            }
+        }
+        return "_";
     }
 
     override status(): string {
-        // TODO
-        // возвращает либо строку с результатом игры, либо, 
-        //   если игра не закончена, вызывает status родителя.
-        return ""
+        const winner = this.checkWin();
+        if (winner !== "_") {
+            return `Победил: ${winner}`;
+        }
+        if (this.isFill()) {
+            return "Ничья!";
+        }
+        return super.status();
     }
-
 }
 
 export const BoardTicParam: BoardParam = {
